e15f7f9ac5aed823874e860549fc9a164acbe5b6
Harsha <sriharsha.mutakodoor@gmail.com>
2022-07-15 00:00:43 +0530

    create linked list

--- src/Board/Board.jsx
+++ src/Board/Board.jsx
@@ -1,24 +1,60 @@
 import React, { useState } from 'react'
 import './Board.css';
-const BOARD_SIZE=10;
 
- const Board = () => {
-    const[board,setBoard]=useState(new Array(BOARD_SIZE).fill(0).map(()=> new Array(BOARD_SIZE).fill(0)),
-    );
-   
+class LinkedListNode{
+  constructor(value){
+    this.value=value;
+    this.next=null;
+  }
+}
+
+class SinglyLinkedList{  //holds references to head & tail//
+  constructor(value){
+    const node=new LinkedListNode(value);
+    this.head=node;
+    this.tail=node;
+  }
+}
+
+
+
+const BOARD_SIZE=10;
+const Board = () => {
+    const[board,setBoard]=useState(createBoard(BOARD_SIZE));
+    const [snakeCells,setSnakeCells]=useState(new Set([44])); 
+    const[snake,setSnake]=useState(new SinglyLinkedList(44))
 
   return (
-    <div className='board'>
-    {
-    board.map((row,rowIdx)=>(
-        <div key={rowIdx} className="row">{
-            row.map((cell,cellIdx)=>(
-                <div key={cellIdx} className="cell"></div>
-            ))
-        }</div>
-    ))}
-        
+    <div className='board'>    
+    {board.map((row,rowIdx)=>(    //row =board[0][0], rowIdx=0// visualize//
+        <div key={rowIdx} className="row">
+            {row.map((cellValue,cellIdx)=>(
+                <div
+                key={cellIdx}
+                className={
+                  `cell ${
+                    snakeCells.has(cellValue) ? 'snake-cell' : ''
+                  }`}                  
+                >{cellValue}</div>
+                
+            ))}        
+        </div>
+    ))}          
     </div>
   );
-}
+};
+
+const createBoard= (BOARD_SIZE)=>{
+
+  let counter=1;
+  const board=[];
+  for(let row=0;row<BOARD_SIZE;row++){
+    const currentRow=[];
+    for(let col=0;col<BOARD_SIZE;col++){
+      currentRow.push(counter++);
+    }
+    board.push(currentRow);
+  }
+  return board;
+};
 export default Board;
